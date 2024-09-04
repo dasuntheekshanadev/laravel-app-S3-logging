fca4d9fdcc99a13685c6413c9bd39b2b7dbcbb98
dasuntheekshanadev <dasuntheekshana12@gmail.com>
2024-09-04 18:34:43 +0530

    New Log Route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/generate-logs', function () {
+    Log::info('This is a test log entry.');
+    return 'Log entry created.';
+});
